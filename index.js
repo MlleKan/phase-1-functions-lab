a1a0ad867470e7981c4c14055326ef987b2a5a57
MlleKan <Kan.dawson@protonmail.com>
2021-11-07 15:13:42 -0500

    commit tree 8ff603ea30d4cc10ebf3b30947f8d4e528bdb0a7

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
 // Code your solution in this file!
+
+// function factorial(num) {
+//     if (num < 0) return undefined;
+//     if (num == 0) return 1;
+//     let x = num - 1;
+//     while (x > 1) {
+//       num *= x;
+//       x--;
+//     }
+//     return num;
+//   }
+
+function distanceFromHqInBlocks(pickup) {
+    if ( pickup > 42 ) {
+    return pickup - 42
+    }
+
+    else {
+        return 42 - pickup
+    }
+
+}
+distanceFromHqInBlocks(43);
+
+
+function distanceFromHqInFeet(pickup) {
+    if ( pickup > 42){
+        return (pickup - 42)*264
+    }
+    else{
+        return (42 - pickup)*264
+    }
+}
+distanceFromHqInFeet(50)    
+    
+function distanceTravelledInFeet(start, end) {
+   if (start > 42) {
+       return (end - start) * 264
+   }
+   else {
+       return (start - end )* 264
+   }
+
+}
+distanceTravelledInFeet(28,34)
+
+
+
+function calculatesFarePrice(start,end){
+    const feet = distanceTravelledInFeet(start,end)
+    if (feet <=  400){
+        return 0
+    }else if (feet > 400 && feet <=2000 ){
+        return (feet - 400) * .02
+    }else if (feet > 2000 && feet < 2500){
+        return 25
+    }else{
+        return 'cannot travel that far'
+    }
+}
+
+
+
